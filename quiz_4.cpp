4f3fb93740a0741a4e66bbe808bfed06dfe96cd7
Adrian Ramirez Mattey <aramirezmattey@gmail.com>
2022-03-17 19:51:11 -0600

    creo que está hecho

--- quiz_4.cpp
+++ quiz_4.cpp
@@ -13,6 +13,7 @@
 
 using namespace std;
 
+// este struct me sirve para alinear todos los elementos en un solo arreglo pero manteniendo de cuál dirección provienen
 typedef struct costDirection {
     int cost;
     int dirSize;
@@ -23,11 +24,13 @@ costDirection costList[1000];
 int cD_comparator(const void *v1, const void *v2) {
     const costDirection *p1 = (costDirection *)v1;
     const costDirection *p2 = (costDirection *)v2;
+    /*
     cout << "p1: " + to_string(p1->cost) + " " + p1->dir + " " + to_string(p1->dirSize)
     << endl
     << "p2: " + to_string(p2->cost) + " " + p2->dir + " " + to_string(p2->dirSize)
     << endl
     ;
+    */
     if (p1->cost < p2->cost) {
         return +1;
     }
@@ -48,29 +51,54 @@ int cD_comparator(const void *v1, const void *v2) {
 
 int costo(int N, int M, int x[], int y[]) {
     // la idea es que siempre la cantidad de cortes será (NxM - 1)
-    // idea matemática de https://math.stackexchange.com/questions/1896374/minimum-number-of-ways-of-dividing-an-m-times-n-chocolate-bar-into-square-piec
+    // para minimizar los cortes se tienen que priorizar primero aquellos que tengan mayor coste
+    // entonces si hay un max de costo en x o y, se hace ese primero, luego se hace el siguiente corte más caro,
+    // en 2 cortes de igual costo se prioriza el corte que esté asociado al array más largo de los 2 de entrada
+    // de esta manera se hace la minimización de costos
+    // mantienen el tamaño de los arreglos x e y
     int x_size = M - 1, y_size = N - 1;
+    // cuentan cuantas sub-divisiones existen
     int Cx = 1, Cy = 1;
+    // tiene track del tamaño de la lista a manejar
     int sizeCounter = 0;
+    // track del costo
+    int cost = 0;
+    // llena el arreglo
     for (int i = 0; i < x_size; i++) {
         costList[i] = {x[i], x_size, "x"};
         sizeCounter++;
     }
+    // llena el arreglo
     for (int i = sizeCounter; i < y_size + x_size; i++) {
         costList[i] = {y[i-x_size], y_size, "y"};
+        sizeCounter++;
     }
-    for (costDirection c : costList) {
-        if (c.cost != 0) {
-            cout << c.cost << c.dir << endl;
+    // sortea para hacer los calculos usando la función de comparación definida arriba
+    qsort(&costList[0], sizeCounter, sizeof(costDirection), cD_comparator);
+
+    // itera por la selección de los elementos sorteados
+    for (int i = 0; i < sizeCounter; i++) {
+
+        if (costList[i].dir.compare("y") == 0) {
+            // es un corte en Y
+            // aumente por el costo multiplicado por las sub-divisiones en X
+            cost += (costList[i].cost * Cx);
+            Cy++;
         }
-    }
-    cout << "after" << endl;
-    qsort(&costList[0], sizeCounter, sizeof(costDirection), cD_comparator );
-    for (costDirection c : costList) {
-        if (c.cost != 0) {
-            cout << c.cost << c.dir << endl;
+        else {
+            // es un corte en X
+            // aumente por el costo multiplicado por las sub-divisiones en Y
+            cost += (costList[i].cost * Cy);
+            Cx++;
         }
+
     }
+
+    return cost;
+}
+
+int costo2(int N, int M, int x[], int y[]) {
+
 }
 
 void radixSort(int *arr, int n, int max) {
@@ -105,7 +133,7 @@ void radixSort(int *arr, int n, int max) {
     // max(12n + 166) tiempos
     // f(n) = max(12n + 166)
     // O(n) = O(nm)
-    // loop invariant
+    // loop invariant => pocket[10] siempre tiene 10 elementos pero cuando está vacío le dice al algoritmo que ya terminó de ingresar elementos
 }
 
 int main () {
@@ -113,6 +141,7 @@ int main () {
     pruebaX2;
     pruebaY2;
     int resCosto = costo(4,5,X,Y);  // expected: 79
+    cout << "result: " + to_string(resCosto) << endl;
 
     return 0;
 }
